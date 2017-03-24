6a04aa6322445c8cfea7229f486ad85dbe24f342
Liora <liorasarahferrero@gmail.com>
2017-03-24 04:06:47 -0400

    8

---
+++ test_operands.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "operands.h" /* gret -rn "word to search" */
+#include "externs.h"
+
+void test_check_operation()
+{
+	char * opcode = "mov";
+	char * rest_of_line1 = " r1,r2 \n";
+	int line_num = 0, result;
+	result = check_operation(opcode, rest_of_line1, line_num);
+
+	printf(" Test for: %s %s - result = %d \n", opcode, rest_of_line1, result);
+	printf(" and IC = %d \n", MAIN_DATA.IC);  
+}
+
+int main()
+{
+	test_check_operation();
+	return 0;
+}
+
