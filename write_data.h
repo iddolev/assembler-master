c9a7f70266868ef877616921fdfa00de305579e4
Iddo Lev <iddolev9@gmail.com>
2017-03-30 15:44:59 +0300

    Update write_data.h

--- write_data.h
+++ write_data.h
@@ -1,3 +1,5 @@
+/* delete old output files (if exist) */
 void clear_output_files(char *file_prefix);
-void write_data_to_files(char *filename);
 
+/* write the output files */
+void write_data_to_files(char *filename);
